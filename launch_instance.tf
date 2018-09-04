e0b6ef5090f8fd1bf1bf67d8851cad1ae13fc4ac
root <root@Anonymous.localdomain>
2018-09-04 15:51:02 +0200

    add-packer et terra

---
+++ launch_instance.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "AKIAIZKMF3J2E3Q4MPMA"
+  secret_key = "Iemoneamepahz4he"
+  region = "us-east-1"
+}
+
+#resource "aws_instance" "example" {
+#  ami           = "ami-2757f631"
+#  instance_type = "t2.micro"
+#}
